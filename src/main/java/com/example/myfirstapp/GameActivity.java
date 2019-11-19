0ea0bf4fe645045205ab0aea2b784bb6631c1883
Hod <hod.gohasi@hotmail.com>
2019-11-19 14:07:20 +0200

    third commit

--- src/main/java/com/example/myfirstapp/GameActivity.java
+++ src/main/java/com/example/myfirstapp/GameActivity.java
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.GridLayout;
 import android.widget.GridView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -19,95 +22,48 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 public class GameActivity extends AppCompatActivity {
+    private Handler myHandler ;
+    private HandlerThread handlerThread=new HandlerThread("HandlerThread");
+    private Thread firstThread;
+    private Thread secondThread;
+    private Thread thirdThread;
+    private int NUM_OF_COL = 3;
+    private Button player;
+    private Button btnLeft;
+    private Button btnRight;
+    private GridLayout gl;
+    private View enemyView;
+    private float posY;
+    private int life=3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        //create handler thread
+        handlerThread.start();
+        myHandler=new Handler(handlerThread.getLooper());
+
+        player = findViewById(R.id.btn_player);
+        btnLeft = findViewById(R.id.move_left);
+        btnRight = findViewById(R.id.move_right);
+        gl = (GridLayout) findViewById(R.id.grid_layout);
+        enemyView = (View) findViewById(R.id.enemy_view);
+
+        //move out of the grid layout
+
+        enemyView.setY(gl.getHeight());
 
         //stop game and going to end screen
         findViewById(R.id.btn_stop).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent gameActivityIntent=new Intent(GameActivity.this,EndActivity.class);
+                Intent gameActivityIntent = new Intent(GameActivity.this, EndActivity.class);
                 startActivity(gameActivityIntent);
-            }
-        });
-        final int NUM_OF_COL=3;
-        final Button player=findViewById(R.id.btn_player);
-        final Button btnLeft=findViewById(R.id.move_left);
-        final Button btnRight=findViewById(R.id.move_right);
-    //exmaple from web
-
-
-        final GridView gv = (GridView) findViewById(R.id.gv);
-
-        // Initializing a new String Array
-        String[] plants = new String[]{
-                "Catalina ironwood",
-                "Cabinet cherry",
-                "Pale corydalis",
-                "Pink corydalis",
-                "Belle Isle cress",
-                "Land cress",
-                "Orange coneflower",
-                "Coast polypody",
-                "Water fern"
-        };
-
-        // Populate a List from Array elements
-        final List<String> plantsList = new ArrayList<String>(Arrays.asList(plants));
-
-        // Data bind GridView with ArrayAdapter (String Array elements)
-        gv.setAdapter(new ArrayAdapter<String>(
-                this, android.R.layout.simple_list_item_1, plantsList) {
-            @Override
-            public boolean isEnabled(int position) {
-                // Disable the third item of GridView
-                if (position == 2) {
-                    return false;
-                } else {
-                    return true;
-                }
-            }
-
-            @Override
-            public View getView(int position, View convertView, ViewGroup parent) {
-                // Return the GridView current item as a View
-                View view = super.getView(position, convertView, parent);
-
-                // Convert the view as a TextView widget
-                TextView tv = (TextView) view;
-
-                // Set the GridView third item text color different
-                if(position == 2){
-                    // set the GridView disable item color
-                    tv.setTextColor(Color.DKGRAY);
-
-                    tv.setVisibility(View.GONE);
-                }
-                else {
-                    // set the TextView text color (GridView item color)
-                    tv.setTextColor(Color.BLACK);
-                    tv.setVisibility(View.VISIBLE);
-                    tv.setBackgroundColor(Color.RED);
-                }
-
-                // Return the TextView widget as GridView item
-                return tv;
-            }
-        });
-
-        gv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // Get the GridView selected/clicked item text
-                String selectedItem = parent.getItemAtPosition(position).toString();
-
-                // Display the selected/clicked item text
-                Toast.makeText(getApplicationContext(), selectedItem,Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -116,8 +72,8 @@ public class GameActivity extends AppCompatActivity {
         btnLeft.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(player.getX()>=360)
-                    player.setX(player.getX()-getResources().getDisplayMetrics().widthPixels/NUM_OF_COL);
+                if (player.getX() >= 360)
+                    player.setX(player.getX() - getResources().getDisplayMetrics().widthPixels / NUM_OF_COL);
 
             }
         });
@@ -125,11 +81,87 @@ public class GameActivity extends AppCompatActivity {
         btnRight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(player.getX()<720)
-                    player.setX(player.getX()+ getResources().getDisplayMetrics().widthPixels/NUM_OF_COL);
-                Log.d("state", "onClick: "+ getResources().getDisplayMetrics().widthPixels/NUM_OF_COL);
+                if (player.getX() < 720)
+                    player.setX(player.getX() + getResources().getDisplayMetrics().widthPixels / NUM_OF_COL);
+                Log.d("state", "onClick: " + getResources().getDisplayMetrics().widthPixels / NUM_OF_COL);
 
             }
         });
+
+
+        //select random initial enemy
+        int random_col;
+
+        enemyView.setBackgroundColor(Color.RED);
+
+
+        switch(NUM_OF_COL) {
+            case 3:
+                while (life > 0) {
+                    checkLife();
+                    random_col = (int) (Math.random() * (NUM_OF_COL - 1));
+                    if (random_col == 0) {
+                        firstColThread();
+                    } else if (random_col == 1) {
+                        secondColThread();
+                    } else if (random_col == 2) {
+                        thirdColThread();
+                    }
+                }
+        }
+
+
     }
+
+    private void checkLife() {
+    }
+
+    private void thirdColThread() {
+    }
+
+    private void secondColThread() {
+    }
+
+    private void firstColThread(){
+        //move down the enemies
+        firstThread=new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+                    Thread.sleep(500);
+                    myHandler.post(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            while (enemyView.getY() < gl.getHeight()) {
+
+                                changePos();
+                                try {
+                                    Thread.sleep(1000);
+                                } catch (Exception c) {
+
+                                }
+                            }
+                        }
+                    });
+
+
+                } catch (Exception c) {
+                }
+
+            }
+        });
+        firstThread.start();
+
+    }
+
+    //move down
+    public void changePos() {
+
+
+        posY += 130;
+        enemyView.setY(posY);
+    }
+
 }
