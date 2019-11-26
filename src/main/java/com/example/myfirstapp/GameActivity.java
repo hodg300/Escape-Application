2604d9fbc180d3d7195b54a6d29fb299fa245805
Hod <hod.gohasi@hotmail.com>
2019-11-26 16:22:23 +0200

    third commit

--- src/main/java/com/example/myfirstapp/GameActivity.java
+++ src/main/java/com/example/myfirstapp/GameActivity.java
@@ -1,19 +1,24 @@
 package com.example.myfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.solver.widgets.Rectangle;
 
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.Message;
 import android.util.Log;
+import android.view.Display;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -34,15 +39,11 @@ import java.util.Random;
 public class GameActivity extends AppCompatActivity {
     private int NUM_OF_COL = 3;
     private View player;
-    private Button btnLeft;
-    private Button btnRight;
-    private LinearLayout main_layout;
-    private LinearLayout l_layout1;
-    private LinearLayout l_layout2;
-    private LinearLayout l_layout3;
     private View enemy1;
     private View enemy2;
     private View enemy3;
+    private Button btnLeft;
+    private Button btnRight;
     private ImageView life_status1;
     private ImageView life_status2;
     private ImageView life_status3;
@@ -50,6 +51,8 @@ public class GameActivity extends AppCompatActivity {
     private ValueAnimator animation1;
     private ValueAnimator animation2;
     private ValueAnimator animation3;
+    private int screenHeight;
+
 
 
     @Override
@@ -62,19 +65,21 @@ public class GameActivity extends AppCompatActivity {
         player = (View) findViewById(R.id.player);
         btnLeft = findViewById(R.id.move_left);
         btnRight = findViewById(R.id.move_right);
-        main_layout=(LinearLayout) findViewById(R.id.main_layout);
-        l_layout1 = (LinearLayout) findViewById(R.id.linear1);
-        l_layout2 = (LinearLayout) findViewById(R.id.linear2);
-        l_layout3 = (LinearLayout) findViewById(R.id.linear3);
         enemy1 = (View) findViewById(R.id.enemy1);
         enemy2 = (View) findViewById(R.id.enemy2);
         enemy3 = (View) findViewById(R.id.enemy3);
         life_status1 = (ImageView) findViewById(R.id.life_status1);
         life_status2 = (ImageView) findViewById(R.id.life_status2);
         life_status3 = (ImageView) findViewById(R.id.life_status3);
-        Log.d("hod", "onCreate: " + main_layout.getHeight());
 
 
+
+        //get screenHeight
+        WindowManager wm=getWindowManager();
+        Display disp= wm.getDefaultDisplay();
+        Point size=new Point();
+        disp.getSize(size);
+        screenHeight=size.y;
 
         //stop game and going to end screen
         findViewById(R.id.btn_stop).setOnClickListener(new View.OnClickListener() {
@@ -110,81 +115,93 @@ public class GameActivity extends AppCompatActivity {
             }
         });
 
-        // getHeight dont het me the real height checkkkk it!!!
-        animation1 = ValueAnimator.ofFloat(-130f,1500f);
-        animation1.setDuration(4000).setRepeatCount(Animation.INFINITE);
+        //create animation
+        animation1 = ValueAnimator.ofInt(-130,screenHeight);
+        animation1.setDuration(10000).setRepeatCount(Animation.INFINITE);
         animation1.start();
         animation1.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator updatedAnimation) {
-                // You can use the animated value in a property that uses the
-                // same type as the animation. In this case, you can use the
-                // float value in the translationX property.
-                float animatedValue = (float)updatedAnimation.getAnimatedValue();
+
+                int animatedValue = (int)updatedAnimation.getAnimatedValue();
                 enemy1.setTranslationY(animatedValue);
-
-
+//              if(isCollision(enemy1,player))
+//                    hitCheck();
             }
         });
-        animation2 = ValueAnimator.ofFloat(-130f,1500f);
-        animation2.setDuration(3000).setRepeatCount(Animation.INFINITE);
+        animation2 = ValueAnimator.ofInt(-130,screenHeight);
+        animation2.setDuration(5000).setRepeatCount(Animation.INFINITE);
         animation2.start();
         animation2.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator updatedAnimation) {
-                // You can use the animated value in a property that uses the
-                // same type as the animation. In this case, you can use the
-                // float value in the translationX property.
-                float animatedValue = (float)updatedAnimation.getAnimatedValue();
+
+                int animatedValue = (int)updatedAnimation.getAnimatedValue();
 
                 enemy2.setTranslationY(animatedValue);
-
-
+//              if(isCollision(enemy2,player))
+//                    hitCheck();
             }
         });
 
-        animation3 = ValueAnimator.ofFloat(-130f,1500f);
-        animation3.setDuration(5000).setRepeatCount(Animation.INFINITE);
+        animation3 = ValueAnimator.ofInt(-130,screenHeight);
+        animation3.setDuration(8000).setRepeatCount(Animation.INFINITE);
         animation3.start();
         animation3.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator updatedAnimation) {
-                // You can use the animated value in a property that uses the
-                // same type as the animation. In this case, you can use the
-                // float value in the translationX property.
-                float animatedValue = (float)updatedAnimation.getAnimatedValue();
-                Log.d("enemy3", "onAnimationUpdate: " + enemy3.getY() + " " + player.getX() +" " +player.getY());
+
+                int animatedValue = (int)updatedAnimation.getAnimatedValue();
                 enemy3.setTranslationY(animatedValue);
-                hitCheck(enemy3);
+                if(isCollision(enemy3,player))
+                    hitCheck();
+
             }
         });
 
+
     }
 
-
-//        //here there is a problem , i need to check getx and gety
-    private void hitCheck(View v) {
-        if (v.getX() == player.getX() && v.getY()>(player.getY() - player.getHeight())) {
-            switch (life) {
-                case 1:
+    private void hitCheck() {
+            this.life--;
+                if (life == 0) {
                     life_status1.setVisibility(View.INVISIBLE);
                     animation1.pause();
                     animation2.pause();
                     animation3.pause();
                     Intent gameActivityIntent = new Intent(GameActivity.this, EndActivity.class);
                     startActivity(gameActivityIntent);
-                    break;
-                case 2:
+                } else if (life == 1) {
                     life_status2.setVisibility(View.INVISIBLE);
-                    this.life--;
-                    break;
-                case 3:
+
+                } else if (life == 2) {
                     life_status3.setVisibility(View.INVISIBLE);
-                    this.life--;
-                    break;
-            }
-        }
+
+                }
+
         return;
+    }
+
+    private boolean isCollision(View e,View p) {
+        int[] enemy_locate = new int[2];
+        int[] player_locate = new int[2];
+
+        e.getLocationOnScreen(enemy_locate);
+        p.getLocationOnScreen(player_locate);
+
+        Log.d("enemy", "isCollision enemy : " + e.getX() +" " + e.getY());
+        Log.d("player", "isCollision player : " + p.getX() +" " + p.getY());
+//        Log.d("sis", "isCollosion1: " + enemy_locate[0] + " " + enemy_locate[1]);
+//        Log.d("sis", "isCollosion2: " + player_locate[0] + " " + player_locate[1]);
+        Rect rect1=new Rect(enemy_locate[0],enemy_locate[1],e.getRight(),e.getHeight()+enemy_locate[0]);
+        Log.d("rect1", "isCollision: " + enemy_locate[0] + " " +enemy_locate[1] + " " + e.getWidth() + " "+ e.getHeight()+" " + enemy_locate[0] );
+        Rect rect2=new Rect(player_locate[0],player_locate[1],p.getRight(),p.getHeight()+player_locate[0]);
+        Log.d("rect2", "isCollision: " + player_locate[0] + " " +player_locate[1] + " " + p.getWidth() + " "+ p.getHeight()+" " + player_locate[0] );
+
+
+        Log.d("answer", "isCollision: " + Rect.intersects(rect1,rect2));
+
+        return Rect.intersects(rect1,rect2);
     }
 }
 
