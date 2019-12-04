7b166daeadff29ba5c4de81ac0049ca8318bb25f
Hod <hod.gohasi@hotmail.com>
2019-12-04 18:40:43 +0200

    fix

---
+++ app/src/main/java/com/example/myfirstapp/EndActivity.java
@@ -0,0 +1,35 @@
+package com.example.myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class EndActivity extends AppCompatActivity {
+    private TextView scoreView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_end);
+
+        scoreView=findViewById(R.id.your_score);
+        Intent intent=getIntent();
+        scoreView.setText("YOUR SCORE : " + intent.getIntExtra("score",0));
+    }
+
+    //press End Game to finish the game and destroy the progress
+    public void clickExit(View view) {
+        moveTaskToBack(true);
+        System.exit(1);
+    }
+
+    public void cliclToRestart(View view) {
+        Intent gameActivityIntent=new Intent(EndActivity.this,GameActivity.class);
+        startActivity(gameActivityIntent);
+    }
+}
